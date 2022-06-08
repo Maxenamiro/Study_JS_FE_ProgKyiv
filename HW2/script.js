23ad7666db7a5e4b2ba9d32a4ae271bc1401086b
Maxenamiro <maxenamiro@gmail.com>
2022-06-08 23:28:02 +0300

    ternarnyi & for

---
+++ HW2/script.js
@@ -0,0 +1,87 @@
+document.write('прямокутник' + '<br>' + '<br>')
+
+for (let i = 0; i < 9; i++) {
+	document.write('$   ')
+	for (let j = 0; j < 9; j++) {
+		document.write('$   ')
+	}
+	document.write('<br>')
+}
+
+document.write('прямокутний трикутник' + '<br>')
+
+for (let i = 0; i < 11; i++) {
+	for (let j = 0; j < i; j++) {
+		document.write('$   ')
+	}
+	document.write('<br>')
+}
+
+document.write('рівносторонній трикутник' + '<br>')
+
+for (let i = 0; i < 9; i++) {
+	for (let j = 1; j < 9 - i; j++) {
+		document.write('&nbsp')
+	}
+	for (let f = 1; f <= 2 * i - 1; f++) {
+		document.write('$')
+	}
+	document.write('<br>')
+}
+//писав ріносторонній трикутник в java, код по логіці однаковий,
+//тут трабл у чомусь іншому
+
+document.write('ромб' + '<br>')
+
+for (let i = 1; i < 5; i++) {
+	for (let j = 0 + i; j < 5; j++) {
+		document.write('&nbsp')
+	}
+	for (let j = 1; j < i * 2; j++) {
+		document.write('$')
+	}
+	document.write('<br>')
+}
+for (let i = 5; i >= 0; i--) {
+	for (let j = 0 + i; j < 5; j++) {
+		document.write('&nbsp')
+	}
+	for (let j = 1; j < i * 2; j++) {
+		document.write('$')
+	}
+	document.write('<br>')
+}
+//з ромбом такий самий мем як з трикутником
+document.write('///////////////////////////////////' + '<br>')
+let a, b, result
+a = parseFloat(prompt('1st number'))
+b = parseFloat(prompt('2nd number'))
+result = a + b < 4 ? 'malo/////////////' : 'bagato//////'
+document.write('//////itogo   ' + result)
+document.write('<br>' + '//////////////////////////////////' + '<br>')
+
+let message
+let login = prompt('vvedt login')
+message =
+	login == 'vasya'
+		? 'pryvit'
+		: login == 'director'
+		? 'zdraste'
+		: login == ''
+		? 'no login'
+		: ''
+document.write('login : ' + login + '<br>' + message + '<br>')
+document.write('<br>')
+
+
+let dod = 0;
+let A = parseFloat(prompt('promizhok chislo 1'));
+let B = parseFloat(prompt('promizhok chislo 2'));
+for (let i = A; i < B; i++) {
+    dod += i
+if(i % 2 !==0){
+document.write("Непарні числа : " + i +"<br>");
+}
+}
+document.write("Сума чисел проміжку : " + dod + "<br>")
+document.write("<br>")
