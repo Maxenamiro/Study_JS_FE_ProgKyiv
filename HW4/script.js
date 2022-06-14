43c8d4310fe133ebcba4b6a1e925f63ac84b1258
Maxenamiro <maxenamiro@gmail.com>
2022-06-14 19:02:59 +0300

    functions

--- HW4/script.js
+++ HW4/script.js
@@ -1,3 +1,15 @@
-function test(function, array){
-    
+function map(fn, array) {
+	var arrLenght = array.length
+	var newArr = []
+	for (var i = 0; i < arrLenght; i++) {
+		newArr[i] = fn(array[i])
+	}
+	return newArr
 }
+//
+let age = parseInt(prompt('Введіть свій вік'))
+function checkAge(age) {
+	let result = age > 18 ? true : confirm('Батьки дозволили?')
+	return result
+}
+console.log('age' + checkAge())
