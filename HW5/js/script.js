e159f63fc27b48ae82f791d6a07543702162dac0
Maxenamiro <maxenamiro@gmail.com>
2022-06-17 18:56:59 +0300

    objects

---
+++ HW5/js/script.js
@@ -0,0 +1,12 @@
+const document = {
+	header: '',
+	body: '',
+	footer: '',
+	date: '',
+	app: {
+		header: { text: '' },
+		body: { text: '' },
+		footer: { text: '' },
+		date: { meaning: '' },
+	},
+}
