d357218bd78a4698162d38cf22748fb166f4d32d
Maxenamiro <maxenamiro@gmail.com>
2022-07-14 22:31:53 +0300

    slider

--- HW9/task4/c4de.js
+++ HW9/task4/c4de.js
@@ -1,24 +1,42 @@
-let divSlider = document.createElement('myDiv')
-document.body.append(div)
+/* Устанавливаем индекс слайда по умолчанию */
+let slideIndex = 1
+showSlides(slideIndex)
 
-let img = document.createElement('img')
-div.append(img)
-
-let imgIndex = 0
-let images = [
-	'https://i.kym-cdn.com/entries/icons/mobile/000/037/043/monkey_sony_walkman.jpg',
-	'https://i.imgflip.com/5a4pil.jpg',
-	'http://pm1.narvii.com/7760/af1f89b0123ef55504057eeeda795ac358c41abar1-325-273v2_00.jpg',
-]
-
-let slider = () => {
-	img.setAttribute('src', images[imgIndex])
-	imgIndex++
-	if (imgIndex === images.length) imgIndex = 0
+/* Увеличиваем индекс на 1 — показываем следующий слайд*/
+function nextSlide() {
+	showSlides((slideIndex += 1))
 }
 
-let showImages = () => {
-	setInterval(slider, 3000)
+/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
+function previousSlide() {
+	showSlides((slideIndex -= 1))
 }
 
-showImages()
+/* Устанавливаем текущий слайд */
+function currentSlide(n) {
+	showSlides((slideIndex = n))
+}
+
+/* Функция перелистывания */
+function showSlides(n) {
+	let i
+	let slides = document.getElementsByClassName('item')
+
+	if (n > slides.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex = slides.length
+	}
+
+	/* Проходим по каждому слайду в цикле for */
+	for (let slide of slides) {
+		slide.style.display = 'none'
+	}
+	slides[slideIndex - 1].style.display = 'block'
+}
+
+Resources
+//'https://i.kym-cdn.com/entries/icons/mobile/000/037/043/monkey_sony_walkman.jpg',
+//'https://i.imgflip.com/5a4pil.jpg',
+//'http://pm1.narvii.com/7760/af1f89b0123ef55504057eeeda795ac358c41abar1-325-273v2_00.jpg',
