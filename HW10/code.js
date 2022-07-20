c659a8534f669b170d7706f08ffaa2925f20625a
Maxenamiro <maxenamiro@gmail.com>
2022-07-20 20:33:27 +0300

    calculator

--- HW10/code.js
+++ HW10/code.js
@@ -1,45 +1,99 @@
-let firstNumber, secondNumber
-let calc = '' //число 2
-let counter = 0
-let result
-let memory = ''
-let clearMemory = false
-let finish = false
+const calc = {
+	firstNumber: '',
+	secondNumber: '',
+	result: '',
+	operand: '',
+	addMemory: '',
+	finish: false,
+}
+function show(value, el) {
+	el.value = value
+}
+const getId = (id) => document.getElementById(id)
 
-const buttonsCollection = document.getElementsByClassName('button')
-const screen = document.getElementsByClassName('screen')
-const buttons = Array.from(buttonsCollection)
-
-buttons.map((button) => {
-	button.addEventListener('click', () => {
-		if (button.value == '=') {
-			counter = 0
-			result = eval(calc)
-			return (screen[0].value = result)
+window.addEventListener('DOMContentLoaded', () => {
+	const btn = document.querySelector('.keys'),
+		display = document.querySelector('.display > input')
+	function clear() {
+		calc.firstNumber = ''
+		calc.secondNumber = ''
+		calc.operand = ''
+		display.textContent = 00
+		show(display.textContent, display)
+	}
+	getId('buttonC').onclick = clear
+	btn.addEventListener('click', function (e) {
+		let el = e.target
+		if (!el.classList.contains('button')) return
+		if (el.classList.contains('clear')) return
+		let a
+		if (
+			el.value === '-' ||
+			el.value === '+' ||
+			el.value === '/' ||
+			el.value === '*' ||
+			el.value === 'm+' ||
+			el.value === 'm-' ||
+			el.value === 'mrc'
+		) {
+			calc.operand = el.value
 		}
-
-		if (button.value == 'C') {
-			screen[0].value = '0'
-			return (calc = ''), (result = '')
+		if (calc.secondNumber === '' && calc.operand === '') {
+			calc.firstNumber += el.value
+			show(calc.firstNumber, display)
+		} else if (calc.operand !== '') {
+			calc.secondNumber += el.value
+			show(calc.secondNumber.replace(/[^0-9, .]/g, ''), display)
 		}
-
-        calc += button.value
-		screen[0].value = calc
-
-		if (button.value == '/' ||button.value == '*' ||button.value == '+' ||button.value == '-') {
-            if (counter = 0) {
-                counter++;
-                calc += button.value
-                screen[0].value = calc
-            } else {
-                calc -= button.value
-                screen[0].value = calc
-            }
-        } else {
-            counter = 0
-        }
-        
+		if (
+			el.value === '-' ||
+			el.value === '+' ||
+			el.value === '/' ||
+			el.value === '*' ||
+			el.value === 'm+' ||
+			el.value === 'm-' ||
+			el.value === 'mrc'
+		) {
+			show(calc.firstNumber, display)
 		}
-    )
+		if (el.value === 'm+') {
+			calc.addMemory = display.value
+		} else if (el.value === 'm-') {
+			show(calc.addMemory, display)
+			calc.firstNumber = calc.addMemory
+			calc.secondNumber = ''
+			calc.operand = ''
+			calc.result = ''
+		} else if (el.value === 'mrc') {
+			calc.addMemory = ''
+		}
+		if (el.value === '=') {
+			switch (calc.operand) {
+				case '+':
+					calc.result =
+						parseFloat(+calc.firstNumber) +
+						parseFloat(+calc.secondNumber.replace(/[^0-9, .]/g, ''))
+					break
+				case '-':
+					calc.result =
+						parseFloat(calc.firstNumber) -
+						parseFloat(calc.secondNumber.replace(/[^0-9, .]/g, ''))
+					break
+				case '/':
+					calc.result =
+						parseFloat(calc.firstNumber) /
+						parseFloat(calc.secondNumber.replace(/[^0-9, .]/g, ''))
+					break
+				case '*':
+					calc.result =
+						parseFloat(calc.firstNumber) *
+						parseFloat(calc.secondNumber.replace(/[^0-9, .]/g, ''))
+					break
+			}
+			show(calc.result, display)
+			calc.firstNumber = calc.result
+			calc.secondNumber = ''
+			calc.operand = ''
+		}
+	})
 })
-
